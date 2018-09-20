d917972489a453c36170c1d68cbf5176d804a0b4
HoWingYip <sploooosh884@gmail.com>
2018-09-20 20:35:16 +0800

    Added try-catch block for fun I guess

--- demo_readfile.js
+++ demo_readfile.js
@@ -2,9 +2,13 @@ var fs = require("fs");
 var http = require("http");
 
 http.createServer(function(req, res) {
-  fs.readFile("demofile1.html", function(err, data) {
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.write(data);
-    res.end();
-  });
+  try {
+    fs.readFile("demofile1.html", function(err, data) {
+      res.writeHead(200, {"Content-Type": "text/html"});
+      res.write(data);
+      res.end();
+    });
+  } catch(err) {
+    console.error(err);
+  }
 }).listen(8080);
